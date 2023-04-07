6de05649baf7818075d56924aa9bfdc2761946e2
Subrata Ghosh Dastidar <github.subrata@gmail.com>
2023-04-08 03:45:04 +0530

    complete What is GPT3 part

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,10 @@ function WhatGPT3() {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="whpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature
+          text="WWe so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+          title="What is GPT-3"
+        />
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -15,9 +18,18 @@ function WhatGPT3() {
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+          title="Chatbots"
+        />
+        <Feature
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+          title="Knowledgebase"
+        />
+        <Feature
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+          title="Education"
+        />
       </div>
     </div>
   );
